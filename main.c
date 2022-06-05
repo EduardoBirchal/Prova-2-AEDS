b6661a7930df3da0221d30cc56bd557ac8238e0c
EduardoBirchal <104524053+EduardoBirchal@users.noreply.github.com>
2022-06-05 17:50:19 -0300

    Coloquei função de detectar maior número

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #define SUCESSO 0
 
+int maiorElemento(int *vetor, int n) {
+    int maior = INT_MIN // Maior começa como o menor número possível pra um int, desse jeito pode ter número negativo no vetor
+
+    for (int i = 0; i < n; i++) { // Loop que percorre o vetor
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+    return maior;
+}
+
 int main(int argc, char** argv)
 {
     printf("Hello World!");
